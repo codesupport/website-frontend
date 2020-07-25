7246b3ddb93dad0c070978a35a4037530e36850b
Lambo <12111454+LamboCreeper@users.noreply.github.com>
2020-07-26 00:44:49 +0100

    Setup ESLint and CI.

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+	root: true,
+	parser: "babel-eslint",
+	plugins: [
+		"react"
+	],
+	parserOptions: {
+		ecmaVersion: 6,
+		sourceType: "module",
+		ecmaFeatures: {
+			modules: true,
+			jsx: true
+		}
+	},
+	rules: {
+		"no-unused-vars": "off",
+		"no-invalid-this": "off",
+		"curly": ["error", "multi-line"],
+		"lines-between-class-members": "off",
+		"space-before-function-paren": ["error", {
+			"anonymous": "never",
+			"named": "never",
+			"asyncArrow": "always"
+		}]
+	},
+	extends: [
+		"eslint-config-codesupport"
+	]
+};
