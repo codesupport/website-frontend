0fb34a3b5535f2299cb8ed553744925c5ca337e3
Jason_000 <jasonhall96686@gmail.com>
2021-02-19 23:15:13 +0000

    Revert cors changes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,9 +5,7 @@ import "../../node_modules/uikit/dist/css/uikit.min.css";
 import "../styles/hljs-theme.css";
 import "../styles/style.css";
 
-if (typeof window !== "undefined") {
-	axios.defaults.withCredentials = location.hostname === "localhost";
-}
+axios.defaults.withCredentials = true;
 
 function App({ Component, pageProps }) {
 	return (
