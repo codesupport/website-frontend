9ce07fada0dcb3280e0d66c5da67f89f2a374dab
Lambo <12111454+LamboCreeper@users.noreply.github.com>
2021-03-28 18:54:41 +0100

    Removed unused code.

--- src/pages/cms/article/[path].js
+++ src/pages/cms/article/[path].js
@@ -1,17 +1,14 @@
-import React, { Component, useState, useEffect } from "react";
+import React, { Component } from "react";
 import styled from "styled-components";
+import { withRouter } from "next/router";
 import UIkit from "uikit";
 import Article from "../../../components/molecules/Article";
 import Markdown from "../../../components/atoms/Markdown";
 import ProtectedPageTemplate from "../../../components/templates/ProtectedPageTemplate";
-import Link from "next/link";
 import Container from "../../../components/templates/Container";
 import ImageManager from "../../../components/organisms/ImageManager";
 import Button from "../../../components/atoms/Button";
-import FormLabel from "../../../components/molecules/FormLabel";
-import TextInput from "../../../components/atoms/TextInput";
 import ArticleService from "../../../services/ArticleService";
-import {useRouter, withRouter} from "next/router";
 import EditArticleMetadata from "../../../components/organisms/EditArticleMetadata";
 import Breadcrumb from "../../../components/molecules/Breadcrumb";
 import ArticleRevisionService from "../../../services/ArticleRevisionService";
@@ -35,184 +32,11 @@ const Preview = styled(Article)`
 	border: 1px solid var(--border);
 `;
 
-
 const PreviewTab = {
 	CONTENT: 0,
 	PREVIEW: 1,
 	METADATA: 2
 };
-
-// function ManageArticle() {
-// 	const revisionService = new ArticleRevisionService();
-// 	const router = useRouter();
-// 	const article = new ArticleService();
-// 	const [error, setError] = useState(undefined);
-// 	const [articleData, setArticleData] = useState(undefined);
-// 	const [content, setContent] = useState("");
-// 	const [revisions, setRevisions] = useState([]);
-// 	const [activeRevision, setActiveRevision] = useState({});
-// 	const [tab, setTab] = useState(PreviewTab.CONTENT);
-// 	const id = router.query?.path;
-//
-// 	useEffect(() => {
-// 		(async () => {
-// 			try {
-// 				const data = await article.getArticleById(id);
-//
-// 				setArticleData(data);
-// 				setActiveRevision(data?.revision);
-// 				setError(undefined);
-// 			} catch ({ message }) {
-// 				setError(message);
-// 			}
-// 		})();
-// 	}, []);
-//
-// 	const updateRevision = event => {
-// 		const newRevisionId = +event.target.value;
-// 		const rev = revisions.find(r => r.id === newRevisionId);
-//
-// 		setActiveRevision(rev);
-// 	};
-//
-// 	const saveContent = async () => {
-// 		try {
-// 			const revisionId = await revisionService.createArticleRevision(articleData.id, {
-// 				content,
-// 				description: activeRevision.description,
-// 				tags: activeRevision.tags
-// 			});
-//
-// 			updateRevision({
-// 				target: {
-// 					value: revisionId
-// 				}
-// 			});
-//
-// 			setError(undefined);
-// 		} catch ({ message }) {
-// 			setError(message);
-// 		}
-// 	};
-//
-// 	useEffect(() => {
-// 		(async () => {
-// 			try {
-// 				const data = await article.getArticleRevisions(id);
-//
-// 				setRevisions(data);
-// 				setError(undefined);
-// 			} catch ({ message }) {
-// 				setError(message);
-// 			}
-// 		})();
-// 	}, [articleData, activeRevision]);
-//
-// 	useEffect(() => {
-// 		setContent(activeRevision?.content);
-// 	}, [activeRevision]);
-//
-// 	return (
-// 		<ProtectedPageTemplate page="Manage Article">
-// 			<Container>
-// 				<Breadcrumb links={[
-// 					{ text: "CMS", link: "cms" },
-// 					{ text: "Manage Articles" }
-// 				]} />
-// 				{error && (
-// 					<section className="uk-alert uk-alert-danger">
-// 						<strong>Error:</strong> {error}
-// 					</section>
-// 				)}
-// 				<h1>Manage Article</h1>
-// 				<Layout>
-// 					<main>
-// 						<h2>Editor</h2>
-// 						<ul data-uk-tab>
-// 							<li
-// 								onClick={() => setTab(PreviewTab.CONTENT)}
-// 								className={tab === PreviewTab.CONTENT && "uk-active"}
-// 							>
-// 								<a>
-// 									Content
-// 								</a>
-// 							</li>
-// 							<li
-// 								onClick={() => setTab(PreviewTab.PREVIEW)}
-// 								className={tab === PreviewTab.PREVIEW && "uk-active"}
-// 							>
-// 								<a>
-// 									Preview
-// 								</a>
-// 							</li>
-// 							<li
-// 								onClick={() => setTab(PreviewTab.METADATA)}
-// 								className={tab === PreviewTab.METADATA && "uk-active"}
-// 							>
-// 								<a>
-// 									Metadata
-// 								</a>
-// 							</li>
-// 						</ul>
-// 						{tab === PreviewTab.CONTENT && (
-// 							<section>
-// 								<TextArea onChange={({ target }) => setContent(target.value)} value={content} cols={50} />
-// 								<Button type="button" onClick={saveContent}>
-// 									Save
-// 								</Button>
-// 							</section>
-// 						)}
-// 						{tab === PreviewTab.PREVIEW && (
-// 							<section>
-// 								<Preview className="uk-article">
-// 									<Markdown content={content} />
-// 								</Preview>
-// 							</section>
-// 						)}
-// 						{tab === PreviewTab.METADATA && (
-// 							<EditArticleMetadata
-// 								article={articleData}
-// 								revision={activeRevision}
-// 								updateRevision={updateRevision}
-// 							/>
-// 						)}
-// 					</main>
-// 					<aside>
-// 						<h2>Details</h2>
-// 						<p>
-// 							<strong>Creator:</strong> {articleData?.createdBy?.alias} <br />
-// 							<strong>Created:</strong> {articleData?.createdOn} <br />
-// 							<strong>Last Updated:</strong> {articleData?.updatedOn} <br />
-// 							<strong>Published:</strong> {articleData?.published ? "True" : "False"}
-// 							{articleData?.published && (
-// 								<>
-// 									<strong>Published Revision:</strong> {articleData?.revision?.id}
-// 								</>
-// 							)}
-// 						</p>
-// 						<label className="uk-form-label" htmlFor="revision-selector">
-// 							<strong>Revision</strong>
-// 						</label>
-// 						<div className="uk-form-controls">
-// 							<select className="uk-select" id="revision-selector" onChange={updateRevision}>
-// 								{revisions.length === 0 && (
-// 									<option selected>No Revision Created</option>
-// 								)}
-// 								{revisions.reverse().map(revision => (
-// 									<option selected={revision.id === activeRevision?.id} value={revision.id} key={revision.id}>
-// 										{revision.id} ({revision.createdOn})
-// 									</option>
-// 								))}
-// 							</select>
-// 						</div>
-// 						<br />
-// 						<ImageManager />
-// 					</aside>
-// 				</Layout>
-// 			</Container>
-// 		</ProtectedPageTemplate>
-// 	);
-// }
 
 class ManageArticle extends Component {
 	articleService = new ArticleService();
