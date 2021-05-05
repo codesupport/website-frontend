f664880c1b524a07de826f122bab9e58b26ab480
Jonathan <32264711+JonaVDM@users.noreply.github.com>
2021-05-05 18:54:32 +0200

    Added alert component (#50)

---
+++ src/components/atoms/Alert.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Alert({ children, type, title }) {
+	return (
+		<div className={`uk-alert uk-alert-${type || "warning"}`}>
+			<strong>{title}</strong>
+			{children}
+		</div>
+	);
+}
+
+export default Alert;
