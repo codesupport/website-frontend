dc1860793c452a26704cddea79a972b7e7af70a5
Lambo <12111454+LamboCreeper@users.noreply.github.com>
2022-04-22 20:46:51 +0100

    Create useAPI() hook to easily make authenticated requests to the API.

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import ProtectedPageTemplate from "../../components/templates/ProtectedPageTemplate";
+import useAPI from "../../hooks/useAPI";
 
 function Profile() {
 	const { user } = useAuth0();
+	const { loading, data, error } = useAPI("GET", "protected");
 
 	return (
 		<ProtectedPageTemplate>
 		    <h1>{user?.name}'s Profile</h1>
+			{loading && <p>Loading...</p>}
+			{data && <p>{JSON.stringify(data)}</p>}
+			{error && <p>{error}</p>}
 		</ProtectedPageTemplate>
 	);
 }
