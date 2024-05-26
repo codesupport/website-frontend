379018459a142011ff68e5d0d993459250a420e1
Cole Green <17110935+nox7@users.noreply.github.com>
2024-05-25 23:01:28 -0500

    Overhauls UI/UX of Articles index

--- src/components/molecules/ArticleCard.js
+++ src/components/molecules/ArticleCard.js
@@ -1,31 +1,51 @@
 import React from "react";
 import styled from "styled-components";
-import URLCard from "./URLCard";
 
-const ReadMore = styled("button")`
-  	padding: 5px 15px;
+const ArticleCardWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	background-color: var(--foreground);
+	box-shadow: var(--box-shadow);
+	border-radius: var(--border-radius);
+	overflow: hidden;
+
+	.content{
+		padding: calc(var(--spacer) * 1.5);
+
+		.meta{
+			font-size: small;
+			font-style: italic;
+		}
+	}
+`;
+
+const ReadMore = styled.a`
+  	padding: calc(var(--spacer) / 2) var(--spacer);
   	color: var(--foreground);
 	background-color: var(--cs-blue);
-  	border-radius: 5px;
-	border: 1px solid var(--cs-blue);
-  	cursor: pointer;
+	text-decoration: none;
+	margin-top: auto;
+	transition: opacity 150ms;
   
-  	:hover {
-	  color: var(--text);
-	  background-color: var(--foreground);
+  	&:hover {
+		opacity: 0.8;
+	}
+
+	@media (max-width: 1200px){
+		text-align: center;
 	}
 `;
 
 function ArticleCard({ article }) {
 	return (
-		<URLCard
-			key={article.id}
-			href={`/article/${article.slug}`}
-			title={article.title}
-			description={article.description}
-		>
-			<ReadMore type="button">Read More</ReadMore>
-		</URLCard>
+		<ArticleCardWrapper>
+			<div className="content">
+				<h2 className="fs-3">{article.title}</h2>
+				{(article.author && article.date) && <span className="meta">By {article.author} on {article.date}</span>}
+				{article.description && <p>{article.description}</p>}
+			</div>
+			<ReadMore href={`/article/${article.slug}`}>Read Article</ReadMore>
+		</ArticleCardWrapper>
 	);
 }
 
