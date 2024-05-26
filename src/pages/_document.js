8115cfcb48ae6ce289b6ad8f098d34bea3fe469a
Cole Green <17110935+nox7@users.noreply.github.com>
2024-05-25 22:12:56 -0500

    Adds new imports of Google fonts with preconnect ability

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -17,6 +17,8 @@ class CustomDocument extends Document {
 		return (
 			<Html>
 				<Head>
+					<link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
+					<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"></link>
 					{this.props.styleTags}
 				</Head>
 				<body>
