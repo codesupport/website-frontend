ca0be39ce6acd8ecc843618896d7c9950bc74d39
Cole Green <17110935+nox7@users.noreply.github.com>
2024-05-26 08:50:25 -0500

    Adds missing lang attribute

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -15,7 +15,7 @@ class CustomDocument extends Document {
 
 	render() {
 		return (
-			<Html>
+			<Html lang="en">
 				<Head>
 					<link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
 					<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"></link>
