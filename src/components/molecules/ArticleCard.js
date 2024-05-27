eb5d8d426c2a9ac5083a61a087d7065d954849e3
Cole Green <17110935+nox7@users.noreply.github.com>
2024-05-27 08:25:52 -0500

    Remove vestigial div

--- src/components/molecules/ArticleCard.js
+++ src/components/molecules/ArticleCard.js
@@ -2,24 +2,6 @@ import React from "react";
 import styled from "styled-components";
 import URLCard from "./URLCard";
 
-const ArticleCardWrapper = styled.div`
-	display: flex;
-	flex-direction: column;
-	background-color: var(--foreground);
-	box-shadow: var(--box-shadow);
-	border-radius: var(--border-radius);
-	overflow: hidden;
-
-	.content{
-		padding: calc(var(--spacer) * 1.5);
-
-		.meta{
-			font-size: calc(var(--body-font-size) / 1.25);
-			font-style: italic;
-		}
-	}
-`;
-
 const ReadMore = styled.p`
   	color: var(--cs-blue);
 	font-weight: bold;
