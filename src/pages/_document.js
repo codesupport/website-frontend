f204b0add94667f04b35cf062759e615f23b6cf6
Cole Green <17110935+nox7@users.noreply.github.com>
2024-05-27 07:46:40 -0500

    Swap fonts back to "Raleway" and "Open sans"

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -17,7 +17,7 @@ class CustomDocument extends Document {
 		return (
 			<Html lang="en">
 				<Head>
-					<link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
+					<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"></link>
 					<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"></link>
 					{this.props.styleTags}
 				</Head>
