91440e8809a1212a8c0f938f088f6f9985a89dc0
Gent Jegeni <72141618+geentjegeni@users.noreply.github.com>
2024-11-10 19:44:39 +0100

    Lint auto adjustments

--- src/components/molecules/NavigationMobile.tsx
+++ src/components/molecules/NavigationMobile.tsx
@@ -82,7 +82,7 @@ const StyledLink = styled.a<{ isActive: boolean }>`
   padding: var(--spacer);
 
   ${({ isActive }) =>
-    isActive &&
+	isActive &&
     css`
       background-color: white;
       color: var(--background);
@@ -123,56 +123,58 @@ export interface NavigationMobileProps {
 }
 
 function NavigationMobile({
-  mobileNavigationIsOpen,
-  setMobileNavigationIsOpen,
+	mobileNavigationIsOpen,
+	setMobileNavigationIsOpen
 }: NavigationMobileProps) {
-  const pathname = usePathname();
-  return (
-    <>
-      <MobileBackdrop
-        className={mobileNavigationIsOpen ? "open" : ""}
-        onClick={() => setMobileNavigationIsOpen(false)}
-      />
-      <MobileNavigation className={mobileNavigationIsOpen ? "open" : ""}>
-        <div className="contents">
-          <CloseMobileNavButton
-            type="button"
-            onClick={() => setMobileNavigationIsOpen(false)}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              className="bi bi-x-lg"
-              viewBox="0 0 16 16"
-            >
-              <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
-            </svg>
-            <span>Close</span>
-          </CloseMobileNavButton>
-          <LogoContainer>
-            <Logo alt="CodeSupport Logo" src="/logo.png" draggable="false" />
-          </LogoContainer>
-          <MobilePageLinks>
-            {config.navigationLinks.map((navLink) => {
-              const isActive = pathname === navLink.href;
-              return (
-                <Link
-                  key={navLink.href}
-                  href={navLink.href}
-                  passHref
-                  legacyBehavior
-                >
-                  <StyledLink isActive={isActive}>{navLink.text}</StyledLink>
-                </Link>
-              );
-            })}
-          </MobilePageLinks>
-        </div>
-      </MobileNavigation>
-    </>
-  );
+	const pathname = usePathname();
+
+	return (
+		<>
+			<MobileBackdrop
+				className={mobileNavigationIsOpen ? "open" : ""}
+				onClick={() => setMobileNavigationIsOpen(false)}
+			/>
+			<MobileNavigation className={mobileNavigationIsOpen ? "open" : ""}>
+				<div className="contents">
+					<CloseMobileNavButton
+						type="button"
+						onClick={() => setMobileNavigationIsOpen(false)}
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="16"
+							height="16"
+							fill="currentColor"
+							className="bi bi-x-lg"
+							viewBox="0 0 16 16"
+						>
+							<path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
+						</svg>
+						<span>Close</span>
+					</CloseMobileNavButton>
+					<LogoContainer>
+						<Logo alt="CodeSupport Logo" src="/logo.png" draggable="false" />
+					</LogoContainer>
+					<MobilePageLinks>
+						{config.navigationLinks.map(navLink => {
+							const isActive = pathname === navLink.href;
+
+							return (
+								<Link
+									key={navLink.href}
+									href={navLink.href}
+									passHref
+									legacyBehavior
+								>
+									<StyledLink isActive={isActive}>{navLink.text}</StyledLink>
+								</Link>
+							);
+						})}
+					</MobilePageLinks>
+				</div>
+			</MobileNavigation>
+		</>
+	);
 }
 
 export default NavigationMobile;
